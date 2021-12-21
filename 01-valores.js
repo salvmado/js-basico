a3ddd83d40e4a78af4af70cf612da022ce7d5f01
SalvadorMD <contacto@salvadormacias.com.mx>
2021-12-20 19:55:41 -0600

    valores

---
+++ 01-valores.js
@@ -0,0 +1,43 @@
+/* ------------------ */
+/* Tipos primitivos   */
+/* ------------------ */
+
+// números
+60
+
+// strings o casenas de texto
+// puedo utilizar palabras o frases 
+// pueden ser con comillas dobles o simples
+"Salvador Macías"
+'Salvador Macías'
+
+// booleanos
+// las inventó George Boole, matemático británico 
+// se usan para validar información.
+true 
+false
+
+// valores vacios o empty values
+// es como un placeholder o espacio reservado para un valor
+// algún dato que falta
+null
+
+// valor que no está definido
+undefined
+
+
+/* ------------------ */
+/* Tipos de objetos   */
+/* ------------------ */
+
+// los corchetes indican que es un array
+[1, 2, 3, 4] 
+
+// las llaves indican que es un objeto
+{ nombre: 'Salvador' }
+
+// con el comando typeof puedo saber en consola de que tipo es mi variable
+// ejemplo:
+
+typeof 60
+// el resultado es "number"
