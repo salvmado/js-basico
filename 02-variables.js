7d8ae253b8137074477ae829d87ba464b8890e00
SalvadorMD <contacto@salvadormacias.com.mx>
2021-12-23 19:22:50 -0600

    variables

---
+++ 02-variables.js
@@ -0,0 +1,30 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+// Una variables es un espacio reservado en memoria para guardar algún dato.
+
+// var es una palabra reservada.
+
+var nombre = "José";
+
+// Declaramos la variable
+var nombre;
+// Inicializamos 
+nombre = "Salvador";
+// El signo "=" es el operador para asignar el valor
+
+// Este es un array
+var elementos = ['Computadora', 'Celular'];
+
+// Console es una variable que pertenece al navegador 
+// log es un método de la función
+// y ellamos llamando al celular, porque el índice comienza en cero
+console.log(elementos[1]); 
+
+// Éste es un objeto
+var persona = {
+  nobmre: "Jose",
+  edad: 44;
+}
+
