390232ba226b0fa0e2e17225f152425a4ec6f79e
SalvadorMD <contacto@salvadormacias.com.mx>
2022-01-09 18:11:24 -0600

    notas

---
+++ 08-operadores.js
@@ -0,0 +1,63 @@
+// Operadores 
+
+/* ------------------------------------------------------------- */
+// operadores binarios:
+
+3 + 2
+10 - 5
+10 * 8
+20 / 2
+
+"Salvador " + "Macias" // el signo de + concatena strings
+
+/* ------------------------------------------------------------- */
+// operadores unitarios
+
+! 
+// negación
+
+!false  
+//true
+
+/* ------------------------------------------------------------- */
+// Operadores de asignación 
+
+var a = 1; // El signo = asigna un valor+
+
+/* ------------------------------------------------------------- */
+// Operadores de comparación
+
+3 == "3";
+true
+// Compara si es igual el valor, no el tipo de dato 
+
+3 === "3";
+false
+// Compara si es estrictamente igual con valor y tipo 
+
+5 < 3
+false
+
+5 > 3
+true
+
+5 <= 6 
+true
+
+5 >= 6
+false
+
+a && b // si "a" es verdad y tambin "b" es verdad, entonces se cumple la condición.
+
+a || b // si "a" es verdad o "b" es verdad entonces sucede la condición.
+
+var edad = 44
+edad++  // es para aumentar de una a uno
+edad
+// Resultado: 45
+
+edad += 2  // Incremento por dos
+edad
+// Resultado: 47 
+
+
