0f1b0d7399cf79b7db497c2950e3aa98647bd998
SalvadorMD <contacto@salvadormacias.com.mx>
2022-01-13 18:23:05 -0600

    nota

---
+++ 10-switch.js
@@ -0,0 +1,19 @@
+// Switch es otra forma de generar condiciones 
+// valida de una forma estricta ===
+// Los casos se validan como verdaderos
+
+var numero = 3;
+
+switch (numero) {
+  case 1:
+    console.log("Soy un uno");
+    break;
+  case 2:
+    console.log("Soy un dos");
+    break;
+  case 3:
+    console.log("Soy un tres");
+    break;
+  default:
+    console.log("No soy ninguno de los números que acabas de poner");
+}
